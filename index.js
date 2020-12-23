d0b61e93c5f15c5acc5d3e50cfa966c48680d011
WayneGong <Wayne.Gong.CN@hotmail.com>
2020-12-23 19:45:47 +0800

    update

--- index.js
+++ index.js
@@ -1,84 +1,170 @@
 class Promise {
-    constructor(executor) {
-        this._status = 'pending'
-        this._value = undefined
-        this._fulfilledCallbacks = []
-        this._rejectedCallbacks = []
+  constructor(executor) {
+    this._status = 'pending'
+    this._value = undefined
+    this._fulfilledCallbacks = []
+    this._rejectedCallbacks = []
 
-        try {
-            executor(resolve, reject)
-        } catch (e) {
-            reject(e)
-        }
-
-        let self = this
-        function resolve(val) {
-            self._status = 'fulfilled'
-            self._value = val
-            self._fulfilledCallbacks.forEach(x => x())
-        }
-
-        function reject(val) {
-            self._status = 'rejected'
-            self._value = val
-            self._rejectedCallbacks.forEach(x => x())
-        }
+    const resolvePromise = val => {
+      // 2.1.2.1
+      if (this._status !== 'pending') return
+      this._status = 'fulfilled'
+      this._value = val
+      this._fulfilledCallbacks.forEach(x => x())
     }
 
-    then(onResolve, onReject) {
-        onResolve = typeof onResolve === 'function' ? onResolve : value => value
-        onReject = typeof onReject === 'function' ? onReject : value => value
-
-        return new Promise((resolve, reject) => {
-            switch (this._status) {
-                case 'pending':
-                    this._fulfilledCallbacks.push(asyncGenerator(this, resolve, reject, onResolve, onReject))
-                    this._rejectedCallbacks.push(asyncGenerator(this, resolve, reject, onResolve, onReject))
-                    break
-                case 'fulfilled':
-                case 'rejected':
-                    asyncGenerator(this, resolve, reject, onResolve, onReject)()
-                    break
-            }
-        })
+    const rejectPromise = val => {
+      // 2.1.3.1
+      if (this._status !== 'pending') return
+      this._status = 'rejected'
+      this._value = val
+      this._rejectedCallbacks.forEach(x => x())
     }
 
-    catch(onReject) {
-        this.then(null, onReject)
+    try {
+      executor(resolvePromise, rejectPromise)
+    } catch (e) {
+      rejectPromise(e)
     }
-
-    static defer() {
-        let dfd = {};
-        dfd.promise = new Promise((resolve, reject) => {
-            dfd.resolve = resolve;
-            dfd.reject = reject;
-        });
-        return dfd;
-    }
-
-    static deferred() {
-        return Promise.defer()
-    }
-}
+  }
 
 
-function asyncGenerator(prePromise, resolve, reject, onResolve, onReject) {
-    return () => {
-        setTimeout(() => {
+  then(onFulfilled, onRejected) {
+    const promise2 = new Promise((resolvePromise, rejectPromise) => {
+      onFulfilled = typeof onFulfilled === 'function' ? tryCall(onFulfilled, rejectPromise) : value => value
+      // 2.2.7.4
+      onRejected = typeof onRejected === 'function' ? tryCall(onRejected, rejectPromise) : value => { throw value }
+
+      switch (this._status) {
+        case 'pending':
+          this._fulfilledCallbacks.push(() => {
+            setTimeout(() => {
+              // 2.2.7.4
+              try {
+                const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
+                promiseResolution(promise2, x, resolvePromise, rejectPromise)
+              } catch (e) {
+                rejectPromise(e)
+              }
+            })
+          })
+          this._rejectedCallbacks.push(() => {
+            setTimeout(() => {
+              try {
+                const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
+                promiseResolution(promise2, x, resolvePromise, rejectPromise)
+              } catch (e) {
+                rejectPromise(e)
+              }
+            })
+          })
+          break
+        case 'fulfilled':
+        case 'rejected':
+          setTimeout(() => {
+            // 2.2.1.1
             try {
-                const cb = prePromise._status === 'fulfilled' ? onResolve : reject
-                const res = cb(prePromise._value)
-
-                if (res instanceof Promise) {
-                    res.then(resolve).catch(reject)
-                } else {
-                    resolve(res)
-                }
+              const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
+              promiseResolution(promise2, x, resolvePromise, rejectPromise)
             } catch (e) {
-                reject(e)
+              rejectPromise(e)
             }
-        })
-    }
+          })
+          break
+      }
+    })
+    return promise2
+  }
+
+
+  catch(onRejected) {
+    return this.then(null, onRejected)
+  }
+
+
+  static defer() {
+    let dfd = {};
+    dfd.promise = new Promise((resolvePromise, rejectPromise) => {
+      dfd.resolve = resolvePromise;
+      dfd.reject = rejectPromise;
+    });
+    return dfd;
+  }
+
+
+  static deferred() {
+    return Promise.defer()
+  }
 }
 
+
+function tryCall(fn, handler) {
+  return function (...args) {
+    try {
+      return fn(...args)
+    } catch (e) {
+      handler(e)
+    }
+  }
+}
+
+function promiseResolution(promise, x, resolvePromise, rejectPromise) {
+  // 2.3.1
+  if (promise === x) {
+    rejectPromise(new TypeError("2.3.1: If `promise` and `x` refer to the same object, reject `promise` with a `TypeError' as the reason."))
+  }
+
+  // 2.3.2
+  if (x instanceof Promise) {
+    x.then(resolvePromise)
+    x.catch(rejectPromise)
+  }
+
+  // 2.3.3
+  else if (x && (typeof x === 'object' || typeof x === 'function')) {
+    // 2.3.3.1
+    let then = x.then
+
+    // 2.3.3.3
+    if (typeof then === 'function') {
+      try {
+        then.call(
+          x,
+          // TODO: 2.3.3.3.1
+          y => {
+            promiseResolution(promise, y, resolvePromise, rejectPromise)
+          },
+          // 2.3.3.3.2
+          r => {
+            rejectPromise(r)
+          }
+        )
+      } catch (e) {
+        rejectPromise(x)
+      }
+    }
+
+    // 2.3.3.4
+    else {
+      resolvePromise(x)
+    }
+  }
+
+  // 2.3.4
+  else {
+    resolvePromise(x)
+  }
+}
+
+
+// const promise = new Promise((r, j) => {
+//     setTimeout(() => {
+//         // r('value')
+//         j('reason')
+//     })
+// })
+
+// promise.then(console.log).then(null, console.error)
+
+// console.log(promise)
 module.exports = Promise;
