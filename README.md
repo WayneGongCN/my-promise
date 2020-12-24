c5a4790cc0a728994c51049346b6087d5fba270a
WayneGong <Wayne.Gong.CN@hotmail.com>
2020-12-24 13:34:20 +0800

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# myPromise
+一个对 Promise/A+ 规范的实现。
+
+
+## Promises/A+ 规范
+
+[https://promisesaplus.com/](https://promisesaplus.com/)
+
+
+## Use
+```
+npm install
+
+npm run test
+```
--- index.js
+++ index.js
@@ -6,16 +6,14 @@ class Promise {
     this._rejectedCallbacks = []
 
     const resolvePromise = val => {
-      // 2.1.2.1
-      if (this._status !== 'pending') return
+      if (this._status !== 'pending') return    // 2.1.2.1
       this._status = 'fulfilled'
       this._value = val
       this._fulfilledCallbacks.forEach(x => x())
     }
 
     const rejectPromise = val => {
-      // 2.1.3.1
-      if (this._status !== 'pending') return
+      if (this._status !== 'pending') return    // 2.1.3.1
       this._status = 'rejected'
       this._value = val
       this._rejectedCallbacks.forEach(x => x())
@@ -32,53 +30,28 @@ class Promise {
   then(onFulfilled, onRejected) {
     const promise2 = new Promise((resolvePromise, rejectPromise) => {
       onFulfilled = typeof onFulfilled === 'function' ? tryCall(onFulfilled, rejectPromise) : value => value
-      // 2.2.7.4
-      onRejected = typeof onRejected === 'function' ? tryCall(onRejected, rejectPromise) : value => { throw value }
+      onRejected = typeof onRejected === 'function' ? tryCall(onRejected, rejectPromise) : value => { throw value }   // 2.2.7.4
 
-      switch (this._status) {
-        case 'pending':
-          this._fulfilledCallbacks.push(() => {
-            setTimeout(() => {
-              // 2.2.7.4
-              try {
-                const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
-                promiseResolution(promise2, x, resolvePromise, rejectPromise)
-              } catch (e) {
-                rejectPromise(e)
-              }
-            })
-          })
-          this._rejectedCallbacks.push(() => {
-            setTimeout(() => {
-              try {
-                const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
-                promiseResolution(promise2, x, resolvePromise, rejectPromise)
-              } catch (e) {
-                rejectPromise(e)
-              }
-            })
-          })
-          break
-        case 'fulfilled':
-        case 'rejected':
-          setTimeout(() => {
-            // 2.2.1.1
-            try {
-              const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
-              promiseResolution(promise2, x, resolvePromise, rejectPromise)
-            } catch (e) {
-              rejectPromise(e)
-            }
-          })
-          break
+      let cb = () => {
+        const x = (this._status === 'fulfilled' ? onFulfilled : onRejected)(this._value)
+        promiseResolution(promise2, x, resolvePromise, rejectPromise)
+      }
+      cb = tryCall(cb, rejectPromise)
+
+      if (this._status === 'pending') {
+        this._fulfilledCallbacks.push(() => setTimeout(cb))
+        this._rejectedCallbacks.push(() => setTimeout(cb))
+      } else {
+        setTimeout(cb)
       }
     })
+
     return promise2
   }
 
 
   catch(onRejected) {
-    return this.then(null, onRejected)
+    return this.then(undefined, onRejected)
   }
 
 
@@ -98,73 +71,52 @@ class Promise {
 }
 
 
+function promiseResolution(promise, x, resolvePromise, rejectPromise) {
+  if (promise === x) return rejectPromise(new TypeError("2.3.1: If `promise` and `x` refer to the same object, reject `promise` with a `TypeError' as the reason."))  // 2.3.1
+
+  if (x && (typeof x === 'object' || typeof x === 'function')) {
+    let use = false
+    try {
+      let then = x.then   // 2.3.3.1
+      // 2.3.3.3
+      if (typeof then === 'function') {
+        then.call(
+          x,
+          // 2.3.3.3.1
+          y => {
+            if (use) return
+            use = true
+            promiseResolution(promise, y, resolvePromise, rejectPromise)
+          },
+          // 2.3.3.3.2
+          r => {
+            if (use) return
+            use = true
+            rejectPromise(r)
+          }
+        )
+      } else {
+        resolvePromise(x)   // 2.3.3.4
+      }
+    } catch (e) {
+      if (use) return
+      use = true
+      rejectPromise(e)
+    }
+  } else {
+    resolvePromise(x)   // 2.3.4
+  }
+}
+
+
 function tryCall(fn, handler) {
   return function (...args) {
     try {
       return fn(...args)
     } catch (e) {
-      handler(e)
+      return handler(e)
     }
   }
 }
 
-function promiseResolution(promise, x, resolvePromise, rejectPromise) {
-  // 2.3.1
-  if (promise === x) {
-    rejectPromise(new TypeError("2.3.1: If `promise` and `x` refer to the same object, reject `promise` with a `TypeError' as the reason."))
-  }
-
-  // 2.3.2
-  if (x instanceof Promise) {
-    x.then(resolvePromise)
-    x.catch(rejectPromise)
-  }
-
-  // 2.3.3
-  else if (x && (typeof x === 'object' || typeof x === 'function')) {
-    // 2.3.3.1
-    let then = x.then
-
-    // 2.3.3.3
-    if (typeof then === 'function') {
-      try {
-        then.call(
-          x,
-          // TODO: 2.3.3.3.1
-          y => {
-            promiseResolution(promise, y, resolvePromise, rejectPromise)
-          },
-          // 2.3.3.3.2
-          r => {
-            rejectPromise(r)
-          }
-        )
-      } catch (e) {
-        rejectPromise(x)
-      }
-    }
-
-    // 2.3.3.4
-    else {
-      resolvePromise(x)
-    }
-  }
-
-  // 2.3.4
-  else {
-    resolvePromise(x)
-  }
-}
-
-
-// const promise = new Promise((r, j) => {
-//     setTimeout(() => {
-//         // r('value')
-//         j('reason')
-//     })
-// })
-
-// promise.then(console.log).then(null, console.error)
-
-// console.log(promise)
 module.exports = Promise;
